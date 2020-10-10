e0afee05ae1067c93d9d7f642cb6e54dae2dd3e3
Thanh Dodeur <thanhdodeur@gmail.com>
2020-10-10 09:07:27 +0200

    Updates

---
+++ precache-manifest.3475bbc65f55521feca9e6ad93a7aef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdbd3cc775b901f36974e15eac1faab",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "94dab1fab0b020570def",
+    "url": "/home/static/css/main.12b44155.chunk.css"
+  },
+  {
+    "revision": "4d6b8b5282539d26f5d9",
+    "url": "/home/static/js/2.ad91766e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.ad91766e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94dab1fab0b020570def",
+    "url": "/home/static/js/main.92c1acaf.chunk.js"
+  },
+  {
+    "revision": "327519257ae83559732d",
+    "url": "/home/static/js/runtime-main.1ebb12cf.js"
+  },
+  {
+    "revision": "7a3731cbb8cf53e7c2bf1a0c3923316a",
+    "url": "/home/static/media/main-icon-dark.7a3731cb.svg"
+  }
+]);
