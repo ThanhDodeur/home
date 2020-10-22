6e25772f524e56bb0cad386254f513d7ec4d77e1
Thanh Dodeur <thanhdodeur@gmail.com>
2020-10-22 08:28:05 +0200

    Theme style

--- src/App.js
+++ src/App.js
@@ -20,12 +20,21 @@ function App(props) {
 
     return (
         <div className={`page ${mode}`}>
-            <div className="monogram-container">
+            <div className="monogram-container noselect">
                 <img width="40px" height="40px" src={monogram} className="main-monogram" alt="monogram"/>
                 <div className="contact-container">
-                    <div onClick={() => { openUrl('https://github.com/ThanhDodeur') }}><img width="30px" height="30px" src={(mode === 'dark' ? github_icon_white : github_icon)} alt="github icon"/><span>Personal Github</span></div>
-                    <div onClick={() => { openUrl('https://github.com/ThanhDodeurOdoo') }}><img width="30px" height="30px" src={(mode === 'dark' ? github_icon_white : github_icon)} alt="github icon"/><span>Work Github</span></div>
-                    <div onClick={() => { openUrl('https://be.linkedin.com/in/thanhsondodeur') }}><img width="30px" height="30px" src={(mode === 'dark' ? linkedin_icon_white : linkedin_icon)} alt="linkedin icon"/><span>LinkedIn</span></div>
+                    <div onClick={() => { openUrl('https://github.com/ThanhDodeur') }}>
+                        <img width="30px" height="30px" src={(mode === 'dark' ? github_icon_white : github_icon)} alt="github icon"/>
+                        <span>Personal Github</span>
+                    </div>
+                    <div onClick={() => { openUrl('https://github.com/ThanhDodeurOdoo') }}>
+                        <img width="30px" height="30px" src={(mode === 'dark' ? github_icon_white : github_icon)} alt="github icon"/>
+                        <span>Work Github</span>
+                    </div>
+                    <div onClick={() => { openUrl('https://be.linkedin.com/in/thanhsondodeur') }}>
+                        <img width="30px" height="30px" src={(mode === 'dark' ? linkedin_icon_white : linkedin_icon)} alt="linkedin icon"/>
+                        <span>LinkedIn</span>
+                    </div>
                 </div>
             </div>
             <div role='button' className='light-icon-wrapper clickable noselect' onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}>
